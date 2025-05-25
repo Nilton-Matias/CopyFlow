1526fb82dae642954f20b9e07eaf5f98f1fa3915
NillasIT <nillas.tech@gmail.com>
2025-05-25 18:13:33 +0200

    Add /login route to fix Route [login] not defined error

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function (Request $request) {
+    return response()->json([
+        'message' => 'Não autenticado',
+        'error'   => 'Você deve estar autenticado para acessar este recurso.'
+    ], 401);
+})->name('login');
